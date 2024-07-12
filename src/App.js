f8ba8415d111a4c5a9ec494ee5f8d5c55dd5fa3f
unknown <Yogesh S>
2024-07-12 18:36:53 +0530

    project setup is done

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Home from "containers/Home/index";
+import React from "react";
+
+import "styles/common.scss";
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+};
+
+export default App;
